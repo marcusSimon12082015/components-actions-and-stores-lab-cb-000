94b54b643c82ad821cdff273d01f703a8b5d09a9
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-24 16:56:38 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createStore } from './store';
+
 function render(){
   ReactDOM.render(<InsertTopLevelComponent />, document.getElementById('container'))
 }
