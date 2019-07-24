35a893750d9d2cb5c68fcdbc58264139c0ed3d75
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-24 17:30:56 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ const store = createStore(bandReducer)
 const renderApp = () => {
   ReactDOM.render(<BandInput store={store}/>, document.getElementById('container'))
 }
- 
+
 store.subscribe(renderApp);
 store.dispatch({})
