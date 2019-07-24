58c5d5a880fa2023d5912fcebaaa55041ef97858
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-24 16:55:39 +0000

    Automatically backed up by Learn

--- src/store.js
+++ src/store.js
@@ -2,15 +2,18 @@ import { render } from './index.js'
 
 export default function createStore(reducer){
     let state;
-
+    let listeners = [];
+    
+    
     function dispatch(action){
       state = reducer(state, action)
       console.log(`the action is`)
       console.log(action)
       console.log(`the state is`)
       console.log(state)
+      listeners.forEach(listener => listener())
       render()
-    }
+    };
 
     function getState(){
       return state;
