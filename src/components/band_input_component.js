de016ff4ddca2cd32673b09417a011e1aa8fc949
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-24 17:14:17 +0000

    Automatically backed up by Learn

--- src/components/band_input_component.js
+++ src/components/band_input_component.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 export default class BandInputComponent extends Component {
-  render(
+  render(){
     return(
         <div>
           <form>
@@ -10,5 +10,5 @@ export default class BandInputComponent extends Component {
           </form>
         </div>
     )
-  )
+  }
 }
